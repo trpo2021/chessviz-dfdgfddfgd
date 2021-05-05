8bca1ebaaf09f937482d54d818a8ac3d76871594
dfdgfddfgd <pasha_barashkov@mail.ru>
2021-05-05 07:38:27 +0200

    fix codestyle

--- src/libchess/figure.c
+++ src/libchess/figure.c
@@ -3,8 +3,7 @@
 
 void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
 {
-
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'p')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'p')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'p';
     }
@@ -12,7 +11,7 @@ void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'P';
     }
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'k')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'k')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'k';
     }
@@ -20,7 +19,7 @@ void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'K';
     }
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'q')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'q')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'q';
     }
@@ -28,7 +27,7 @@ void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'Q';
     }
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'n')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'n')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'n';
     }
@@ -36,7 +35,7 @@ void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'N';
     }
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'b')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'b')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'b';
     }
@@ -44,7 +43,7 @@ void figure(char board[9][9], int figuref[9], int figureF[9], char turn[6])
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'B';
     }
-    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'r')){
+    if ((board[(56 - turn[1])][(turn[0] - 96)] == 'r')) {
         board[(56 - turn[1])][(turn[0] - 96)] = ' ';
         board[(56 - turn[4])][(turn[3] - 96)] = 'r';
     }
