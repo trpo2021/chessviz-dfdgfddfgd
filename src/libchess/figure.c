750e856b3ae318d349e8a934707a7a7255e69859
dfdgfddfgd <pasha_barashkov@mail.ru>
2021-05-09 15:51:45 +0200

    fix codestyle

--- src/libchess/figure.c
+++ src/libchess/figure.c
@@ -3,6 +3,7 @@
 
 void figure(char board[9][9], char turn[6])
 {
-        board[(56 - turn[4])][(turn[3] - 96)] = board[(56 - turn[1])][(turn[0] - 96)];
-        board[(56 - turn[1])][(turn[0] - 96)] = ' ';
+    board[(56 - turn[4])][(turn[3] - 96)]
+            = board[(56 - turn[1])][(turn[0] - 96)];
+    board[(56 - turn[1])][(turn[0] - 96)] = ' ';
 }
--- src/libchess/turncheck.c
+++ src/libchess/turncheck.c
@@ -6,9 +6,9 @@ int turncheck(char turn[6])
     int ascii_i = 105;
     int ascii_0 = 48;
     int ascii_9 = 57;
-    if ((turn[0] > ascii) && (turn[0] < ascii_i) && (turn[3] > ascii) && (turn[3] < ascii_i)
-        && (turn[1] > ascii_0) && (turn[1] > ascii_0) && (turn[1] < ascii_9) && (turn[4] > ascii_0)
-        && (turn[4] < ascii_9))
+    if ((turn[0] > ascii) && (turn[0] < ascii_i) && (turn[3] > ascii)
+        && (turn[3] < ascii_i) && (turn[1] > ascii_0) && (turn[1] > ascii_0)
+        && (turn[1] < ascii_9) && (turn[4] > ascii_0) && (turn[4] < ascii_9))
         return 0;
     else
         return 1;
